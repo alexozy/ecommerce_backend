1408670e7f4f7c94ea9cfeaed5bcc30c2f0922a9
Ozanne Alexander <alexanoz@outlook.com>
2023-02-28 20:44:54 -0500

    set mode for product Tag; set up the associations in the index file

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,23 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
