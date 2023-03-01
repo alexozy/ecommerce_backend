a1a8f98ae816cc5bb92be08c6e9c3dcaa2b2f1ad
Ozanne Alexander <alexanoz@outlook.com>
2023-03-01 10:02:16 -0500

    making pushes before askbcs help

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,28 +34,75 @@ router.get('/:id', (req, res) => {
       model: Product,
     }],
   })
-.then((dbData) => res.json(dbData))
-.catch((err) => {
-  console.log(err);
-  res.status(500).json(err);
-});
-
+    .then((dbData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "id has no user data" });
+        return;
+      }
+      res.json(dbData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 
 });
 
 router.post('/', (req, res) => {
   // create a new tag
-  Tag.create
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+  .then((dbData) => res.json(dbData))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-  Tag.update
+  // make sure to include the req.body so you can tie it to it's specific tag name!
+  Tag.update({
+      tag_name: req.body.tag_name,
+    },
+    { where: {
+        id: req.params.id,
+      },
+    },
+  )
+  .then((dbData) => {
+    if (!dbUserData) {
+      res.status(404).json({ message: "id has no user data" });
+      return;
+    }
+    res.json(dbData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
-  Tag.destroy
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+
+  .then((dbData) => {
+    if (!dbUserData) {
+      res.status(404).json({ message: "id has no user data" });
+      return;
+    }
+    res.json(dbData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
